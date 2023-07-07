e4a9895ab47c9e5e7e11cf33773ac672173e6d16
Михаил <102461102+LittleCat1643@users.noreply.github.com>
2023-07-07 16:26:56 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+let button = document.querySelector('button');
+window.guess = {
+    number: Math.floor(Math.random() * 11)
+};
+button.onclick = () => {
+    let number = document.querySelector('input').value;
+    if (number >= 0 && number <= 10) {
+        if (number == guess.number) {
+            let result = document.querySelector('.result > .content');
+            let history = document.querySelector('.history > .content');
+            result.innerHTML = '<p class="green">You guessed it! Refresh the page if you want to play again.</p>';
+            history.innerHTML += '<p class="green">' + number + '</p>';
+            let button = document.querySelector('button');
+            button.disabled = true;
+        } else {
+            let result = document.querySelector('.result > .content');
+            let history = document.querySelector('.history > .content');
+            if (number > guess.number) {
+                result.innerHTML = '<p class="blue">You didn\'t guess right! The number is smaller.</p>';
+                history.innerHTML += '<p class="blue">' + number + '</p>';
+            } else {
+                result.innerHTML = '<p class="red">You didn\'t guess right! The number is bigger.</p>';
+                history.innerHTML += '<p class="red">' + number + '</p>';
+            }
+        }
+    } else {
+        let result = document.querySelector('.result > .content');
+        result.innerHTML = '<p class="error">You entered a number not from the range from 0 to 10!</p>';
+    }
+}
