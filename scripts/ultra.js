0ca9f10221a4757ef5b129c80b1b51ab1c511281
Михаил <102461102+LittleCat1643@users.noreply.github.com>
2023-07-09 12:04:40 +0400

    Create ultra.js

---
+++ scripts/ultra.js
@@ -0,0 +1,45 @@
+let button = document.querySelector('button');
+window.guess = {
+    number: Math.floor(Math.random() * 1000) + 1,
+    attempts: 35
+};
+button.onclick = () => {
+    let number = document.querySelector('input').value;
+    if (number >= 1 && number <= 1000) {
+        if (number == guess.number) {
+            let result = document.querySelector('.result > .content');
+            let history = document.querySelector('.history > .content');
+            result.innerHTML = '<p class="green">Ты угадал! Для того, чтобы попробовать снова, обнови страницу.</p>';
+            history.innerHTML += '<b class="green">' + number + '   </b>';
+            let button = document.querySelector('button');
+            button.disabled = true;
+        } else {
+            let result = document.querySelector('.result > .content');
+            let history = document.querySelector('.history > .content');
+            if (number > guess.number) {
+                guess.attempts = guess.attempts - 1;
+                if (!guess.attempts) {
+                    result.innerHTML = '<p class="red">Ты потратил все попытки. Для того, чтобы попробовать снова, обнови страницу.</p>';
+                    let button = document.querySelector('button');
+                    button.disabled = true;
+                } else {
+                    result.innerHTML = '<p class="blue">Ты не угадал! Загаданное число меньше. Осталось попыток: ' + guess.attempts + '</p>';
+                    history.innerHTML += '<b class="blue">' + number + '   </b>';
+                }
+            } else {
+                guess.attempts = guess.attempts - 1;
+                if (!guess.attempts) {
+                    result.innerHTML = '<p class="red">Ты потратил все попытки. Для того, чтобы попробовать снова, обнови страницу.</p>';
+                    let button = document.querySelector('button');
+                    button.disabled = true;
+                } else {
+                    result.innerHTML = '<p class="red">Ты не угадал! Загаданное число больше. Осталось попыток: ' + guess.attempts + '</p>';
+                    history.innerHTML += '<b class="red">' + number + '   </b>';
+                }
+            }
+        }
+    } else {
+        let result = document.querySelector('.result > .content');
+        result.innerHTML = '<p class="error">Ты ввёл число не от 1 до 1000!</p>';
+    }
+}
